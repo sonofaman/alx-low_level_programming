a11165adb1cef0462810d2be6336fae6ad4c92c6
sonofaman <sonofaman13@gmail.com>
2022-06-02 11:48:20 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,19 @@
+/*
+ *File: 4-puts.c
+ *Auth: Woldeamanuel Abraham
+ */
+
 #include<stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle,followed by a new line.
+ *
+ *Return: always 0.
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-    return 0;
+	return (0);
 }
