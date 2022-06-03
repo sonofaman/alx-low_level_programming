7f312b3ed21effffcaf74b99ea8e1cb07f668543
sonofaman <sonofaman13@gmail.com>
2022-06-03 13:05:45 -0700

    Variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,7 +18,7 @@ int main(void)
 	for (alp = 'a'; alp <= 'z'; alp++)
 		putchar(alp);
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints the lowercase alphabet in reverse.
- *
+ *        Followed by new line.
  * Return: Always 0.
  */
 int main(void)
